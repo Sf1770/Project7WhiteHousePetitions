c55fa7145abb80549abaadf3c0a04bd41fbf851e
Sf1770 <sf1770@nyu.edu>
2018-01-05 17:50:05 -0500

    Initial Commit

---
+++ Project7_WhiteHousePetitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project7_WhiteHousePetitions
+//
+//  Created by Sabrina Fletcher on 1/5/18.
+//  Copyright © 2018 Sabrina Fletcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
